147a6110f159d81199c9fb1c4673b894bab7064c
Ilya Beg <ilyabegichev@gmail.com>
2026-08-05 09:54:04 +0300

    Add comments to the custom serverEventHandlers hook for better understanding

--- src/renderer/hooks/serverEventHandlers.hook.tsx
+++ src/renderer/hooks/serverEventHandlers.hook.tsx
@@ -7,6 +7,8 @@ import { offServerEvent, onServerEvent } from '../IPC/OnServerEvent';
 import { CollisionData } from 'shared/Types/CollisionData';
 
 export default function useServerEventHandlers() {
+
+  // callback functions stored in memory once the app renders (created only once)
   const errorMessageHandler = useCallback(
     (_event: IpcRendererEvent, err: ErrorMessage) => {
       console.error(`Error from server: ${err.stringMessage}`);
@@ -17,8 +19,7 @@ export default function useServerEventHandlers() {
   const collisionAlertHandler = useCallback(
     (_event: IpcRendererEvent, collision: CollisionData) => {
       console.warn(`
-        Collision! Between plane A: '${collision.planeA.id}' and Plane B: '${collision.planeB.id}'
-        at latitude: ${collision.coordinates.lat} - longitude ${collision.coordinates.lon}, 
+        Collision! At latitude: ${collision.coordinates.lat} - longitude ${collision.coordinates.lon}, 
         at time: ${collision.time_of_collision}`
       );
       //<FlightPin lat={collision.coordinates.lat} lon={collision.coordinates.lon} />
@@ -26,6 +27,7 @@ export default function useServerEventHandlers() {
     []
   );
 
+  // a promise that these functions will be disposed when the components die
   useEffect(() => {
     onServerEvent('error_message', errorMessageHandler, false);
     onServerEvent('collision_alert', collisionAlertHandler, false);
