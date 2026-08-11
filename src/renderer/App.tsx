ea412be84f37cde1c6143b6279ccf7b6e6d68f65
Ilya Beg <ilyabegichev@gmail.com>
2026-08-11 15:19:28 +0300

    Add app_state enum to App.tsx for blocking everything when cinfiguring plane objects

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,42 +1,70 @@
 import './App.css';
 import useServerEventHandlers from './hooks/serverEventHandlers.hook';
 import Header from './components/Header';
-
-import { Viewer } from 'resium';
+import { Provider, Viewer } from 'resium';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
 import EarthClickControl from './components/EarthClickHandlers/EarthClickControl';
 import CollisionControl from './components/CollisionComponents/CollisionControl';
+import PlaneConfig from './components/FlightDisplayComponents/PlaneConfig';
+import { useState, createContext } from 'react';
 
+// tell cesium exactly where to find all cesium assets
 (window as any).CESIUM_BASE_URL = '/cesium/';
+
+// application states
+export enum app_state {
+  DEFAULT, CONFIGURING, CLICKING
+}
+
+export interface StateContextProp {
+  appState: app_state,
+  setAppState: React.Dispatch<React.SetStateAction<app_state>>
+}
+
+// create appState context for child elements to updtate the app state.
+// for example: when adding a flight, the state should be CONFIGURING and block everything else
+export const AppStateContext = createContext<StateContextProp | null>(null);
 
 export default function App() {
   useServerEventHandlers();
 
+  // save app state for configuring a flight and adding it to memory
+  const [appState, setAppState] = useState<app_state>(app_state.DEFAULT);
+
   return (
-    <div className="body-container">
+    <>
+      {/* pass the context to children */}
+      <AppStateContext.Provider value={{appState, setAppState}}>
+        <div className="body-container">      
+          {/* app header component */}
+          <Header />
 
-      <Header />
+          <div className="main-content">
+            {/* cesium virtual globe viewer */}
+            <Viewer
+              full 
+              geocoder={false} 
+              baseLayer={false}
+              timeline={true}
+              sceneModePicker={false}
+              infoBox={false} // disables the gray popup box in the top right
+              shouldAnimate={true}
+              >  
 
-      <div className="main-content">
-        <Viewer
-          full 
-          geocoder={false} 
-          baseLayer={false}
-          timeline={true}
-          sceneModePicker={false}
-          infoBox={false} // disables the gray popup box in the top right
-          shouldAnimate={true}
-          >          
+              {/* configuring plane only if state is CONFIGURING */}    
+              <PlaneConfig/>    
 
-          {/* clicking earth logic */}
-          <EarthClickControl />
+              {/* clicking earth logic */}
+              <EarthClickControl />
 
-          {/* collision displayer */}
-          <CollisionControl />
+              {/* collision displayer */}
+              <CollisionControl />
 
-          {/* Your 3D airplane path components will eventually go here */}
-        </Viewer>
-      </div>
-    </div>
+              {/* Your 3D airplane path components will eventually go here */}
+            </Viewer>
+          </div>
+        </div>
+      </AppStateContext.Provider>    
+    </>
   );
 }
--- src/renderer/components/Header.tsx
+++ src/renderer/components/Header.tsx
@@ -1,15 +1,24 @@
-import { useState } from 'react';
+import { useContext } from 'react';
 import '../Styles/Header.css';
-import PlaneConfig from './FlightDisplayComponents/PlaneConfig';
 import ConnectingAirportsIcon from '@mui/icons-material/ConnectingAirports';
+import { AppStateContext, app_state } from '../App';
 
 export default function Header() {
 
-    const [planeConfVisibility, setVisibility] = useState<boolean>(false);
+    // context of the app state
+    const appStateContext = useContext(AppStateContext);
 
+    // start configuring the plane object
     const handleClick = () => {
-        setVisibility(true);
+        if (appStateContext?.appState !== app_state.CONFIGURING)
+            appStateContext?.setAppState(app_state.CONFIGURING);
     };
+
+    // personal note: no need to use useCallback or useMemo hooks here to stop the component
+    // from re-rendering when he changes the appState because using hooks uses more memory
+    // allocation and CPU cycles, and if a component is simple and light weight as this one,
+    // it is sometimes better to just let the component re-render because react is lightning
+    // fast with re-rendering small and simple components.
 
     return (
         <header className="app-header">
@@ -18,7 +27,6 @@ export default function Header() {
             </div>
             <h1 className="header-title">AirPredict</h1>
             <button onClick={handleClick}>Add Flight</button>
-            {planeConfVisibility && <PlaneConfig />}
         </header>
     );
 }
